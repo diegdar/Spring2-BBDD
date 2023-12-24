a00c66c8da20c068893154096313cb32e4db296d
Diego Chacon <diegdarch@hotmail.com>
2023-12-24 11:41:12 +0100

    terminado queries N2-Tienda

--- MySQL/mysql-queries/Tienda/queries_tienda.sql
+++ MySQL/mysql-queries/Tienda/queries_tienda.sql
@@ -32,3 +32,49 @@ SELECT nombre, precio FROM producto ORDER BY nombre ASC, precio DESC;
 SELECT * FROM fabricante LIMIT 5;
 -- 17.	Devuelve una lista con 2 filas a partir de la cuarta fila de la tabla "fabricante". La cuarta fila también debe incluirse en la respuesta.
 SELECT * FROM fabricante LIMIT 3,2;
+-- 18.	Lista el nombre y precio del producto más barato. (Utiliza solo las cláusulas ORDER BY y LIMIT). NOTA: Aquí no podrías usar MIN(precio), necesitarías GROUP BY
+SELECT nombre, precio FROM producto ORDER BY precio ASC LIMIT 1;
+-- 19.	Lista el nombre y precio del producto más caro. (Utiliza solamente las cláusulas ORDER BY y LIMIT). NOTA: Aquí no podrías usar MAX(precio), necesitarías GROUP BY.
+SELECT nombre, precio FROM producto ORDER BY precio DESC LIMIT 1;
+-- 20.	Lista el nombre de todos los productos del fabricante cuyo código de fabricante es igual a 2.
+SELECT p.nombre AS nom_producto FROM producto p  JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.codigo = 2;
+-- 21.	Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los productos de la base de datos.
+SELECT p.nombre AS nom_producto, p.precio, f.nombre AS nom_fabricante FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante;
+-- 22.	Devuelve una lista con el nombre del producto, precio y nombre de fabricante de todos los productos de la base de datos. Ordena el resultado por el nombre del fabricante, por orden alfabético.
+SELECT p.nombre AS nom_producto, p.precio, f.nombre AS nom_fabricante FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante ORDER BY nom_fabricante;
+-- 23.	Devuelve una lista con el código del producto, nombre del producto, código del fabricante y nombre del fabricante, de todos los productos de la base de datos.
+SELECT p.codigo AS codigo_producto, p.nombre AS nom_producto, p.codigo_fabricante AS codigo_fabricante, f.nombre AS nom_fabricante FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante;
+-- 24.	Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más barato.
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo ORDER BY p.precio ASC LIMIT 1;
+-- 25.	Devuelve el nombre del producto, su precio y el nombre de su fabricante, del producto más caro.
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo ORDER BY p.precio DESC LIMIT 1;
+-- 26.	Devuelve una lista de todos los productos del fabricante Lenovo.
+SELECT p.nombre FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre = 'lenovo';
+-- 27.	Devuelve una lista de todos los productos del fabricante Crucial que tengan un precio mayor que 200€.
+SELECT p.nombre FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre = 'Crucial' AND p.precio > 200;
+-- 28.	Devuelve una lista con todos los productos de los fabricantes Asus, Hewlett-Packard y Seagate. Sin utilizar el operador IN.
+SELECT p.nombre FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre = 'Asus' OR f.nombre='Hewlett-Packard' OR f.nombre='Seagate';
+-- 29.	Devuelve un listado con todos los productos de los fabricantes Asus, Hewlett-Packard y Seagate. Usando el operador IN.
+SELECT p.nombre FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre IN('Asus', 'Hewlett-Packard', 'Seagate');
+-- 30.	Devuelve un listado con el nombre y el precio de todos los productos de los fabricantes cuyo nombre acabe por la vocal e.
+SELECT p.nombre, p.precio FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre LIKE '%e';
+-- 31.	Devuelve un listado con el nombre y precio de todos los productos de cuyos fabricantes contenga el carácter w en su nombre.
+SELECT p.nombre, p.precio FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE f.nombre LIKE '%w%';
+-- 32.	Devuelve un listado con el nombre de producto, precio y nombre de fabricante, de todos los productos que tengan un precio mayor o igual a 180 €. Ordena el resultado, en primer lugar, por el precio (en orden descendente) y, en segundo lugar, por el nombre (en orden ascendente).
+SELECT p.nombre, p.precio, f.nombre FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante WHERE p.precio > 180 ORDER BY p.precio DESC, p.nombre ASC;
+-- 33.	Devuelve un listado con el código y el nombre de fabricante, solo de aquellos fabricantes que tienen productos asociados en la base de datos.
+SELECT f.codigo, f.nombre FROM producto p JOIN fabricante f ON f.codigo = p.codigo_fabricante GROUP BY f.codigo;
+-- 34.	Devuelve un listado de todos los fabricantes que existen en la base de datos, junto con los productos que tiene cada uno de ellos. El listado deberá mostrar también a aquellos fabricantes que no tienen productos asociados.
+SELECT f.nombre AS nombre_fabricante, p.nombre AS nombre_producto FROM fabricante f LEFT JOIN producto p ON p.codigo_fabricante = f.codigo;
+-- 35.	Devuelve un listado en el que sólo aparezcan aquellos fabricantes que no tienen ningún producto asociado.
+SELECT f.nombre AS nombre_fabricante FROM fabricante f LEFT JOIN producto p ON p.codigo_fabricante = f.codigo WHERE p.nombre IS NULL;
+-- 36.	Devuelve todos los productos del fabricante Lenovo. (Sin utilizar INNER JOIN).
+SELECT p.nombre AS nombre_producto FROM producto p RIGHT JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre='lenovo' AND p.nombre IS NOT NULL; 
+-- 37.	Devuelve todos los datos de los productos que tienen el mismo precio que el producto más caro del fabricante Lenovo. (Sin utilizar INNER JOIN).
+SELECT p.codigo AS codigo_producto, p.nombre AS nom_producto, p.precio FROM producto p RIGHT JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre='lenovo' ORDER BY p.precio DESC LIMIT 1; 
+-- 38.	Lista el nombre del producto más caro del fabricante Lenovo.
+SELECT p.nombre AS nom_producto FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre='lenovo' ORDER BY p.precio DESC LIMIT 1; 
+-- 39.	Lista el nombre del producto más barato del fabricante Hewlett-Packard.
+SELECT p.nombre AS nom_producto FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre='Hewlett-Packard' ORDER BY p.precio ASC LIMIT 1; 
+-- 40.	Devuelve todos los productos de la base de datos que tienen un precio mayor o igual al producto más caro del fabricante Lenovo.
+SELECT * FROM producto p WHERE p.precio >= ( SELECT MAX(p.precio) FROM producto p WHERE p.codigo_fabricante = (SELECT f.codigo FROM fabricante f WHERE f.nombre='lenovo') ); 
---
+++ MySQL/mysql-queries/Universidad/queries-universidad.sql
@@ -0,0 +1,54 @@
+-- 1.	Devuelve un listado con el primer apellido, segundo apellido y el nombre de todos los alumnos/as. El listado deberá estar ordenado alfabéticamente de menor a mayor por el primer apellido, segundo apellido y nombre.
+SELECT  apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1 ASC, apellido2 ASC , nombre ASC ;
+-- 2.	Averigua el nombre y los dos apellidos de los/as alumnos/as que no han dado de alta su número de teléfono en la base de datos.
+SELECT  nombre, apellido1, apellido2 FROM persona WHERE tipo = 'alumno' AND telefono IS NULL ;
+-- 3.	Devuelve el listado de los alumnos/as que nacieron en 1999.
+SELECT  nombre, apellido1, apellido2 FROM persona WHERE fecha_nacimiento LIKE "%1999%" ;
+-- 4.	Devuelve el listado de profesores/as que no han dado de alta su número de teléfono en la base de datos y además su NIF termina en K.
+SELECT  nombre, apellido1, apellido2 FROM persona WHERE tipo = 'profesor' AND telefono IS NULL AND nif LIKE "%K" ;
+-- 5.	Devuelve el listado de las asignaturas que se imparten en el primer cuatrimestre, en el tercer curso del grado que tiene el identificador 7.
+SELECT a.nombre FROM asignatura a JOIN grado g ON a.id_grado = g.id  WHERE a.cuatrimestre= 1 AND a.curso= 3 AND g.id = 7;
+-- 6.	Devuelve un listado de los profesores/as junto con el nombre del departamento al que están vinculados/as. El listado debe devolver cuatro columnas, primer apellido, segundo apellido, nombre y nombre del departamento. El resultado estará ordenado alfabéticamente de menor a mayor por apellidos y nombre.
+SELECT per.apellido1, per.apellido2, per.nombre AS nom_profesor, dep.nombre AS departamento FROM persona per JOIN profesor prof ON per.id = prof.id_profesor JOIN departamento dep ON dep.id = prof.id_departamento ORDER BY per.apellido1 ASC, per.apellido2 ASC, per.nombre ASC;
+-- 7.	Devuelve un listado con el nombre de las asignaturas, año de inicio y año de fin del curso escolar del alumno/a con NIF 26902806M.
+SELECT asig.nombre, ce.anyo_inicio, ce.anyo_fin FROM asignatura asig JOIN alumno_se_matricula_asignatura am ON am.id_asignatura = asig.id JOIN persona per ON per.id = am.id_alumno JOIN curso_escolar ce ON ce.id = am.id_curso_escolar WHERE per.tipo = 'alumno' AND nif = '26902806M';
+-- 8.	Devuelve un listado con el nombre de todos los departamentos que tienen profesores/as que imparten alguna asignatura en el Grado en Ingeniería Informática (Plan 2015).
+SELECT DISTINCT dep.nombre AS departamento FROM departamento dep JOIN profesor prof ON prof.id_departamento = dep.id JOIN asignatura asig ON asig.id_profesor = prof.id_profesor JOIN grado g ON g.id = asig.id_grado WHERE g.nombre = 'Grado en Ingeniería Informática (Plan 2015)';
+-- 9.	Devuelve un listado con todos los alumnos/as que se han matriculado en alguna asignatura durante el curso escolar 2018/2019.
+SELECT DISTINCT per.nombre, per.apellido1, per.apellido2, am.id_alumno FROM persona per JOIN alumno_se_matricula_asignatura am ON per.id = am.id_alumno JOIN asignatura asig ON asig.id = am.id_asignatura JOIN curso_escolar ce ON ce.id = am.id_curso_escolar WHERE ce.anyo_inicio = '2018' AND ce.anyo_fin='2019';
+	/*LEFT JOIN y RIGHT JOIN */
+-- 1. Devuelve un listado con los nombres de todos los profesores/as y los departamentos que tienen vinculados/as. El listado también debe mostrar aquellos profesores/as que no tienen ningún departamento asociado. El listado debe devolver cuatro columnas, nombre del departamento, primer apellido, segundo apellido y nombre del profesor/a. El resultado estará ordenado alfabéticamente de menor a mayor por el nombre del departamento, apellidos y nombre.
+SELECT dep.nombre AS departamento, per.apellido1, per.apellido2, per.nombre FROM persona per RIGHT JOIN profesor prof ON prof.id_profesor = per.id JOIN departamento dep ON dep.id = prof.id_departamento ORDER BY dep.nombre, per.apellido1, per.apellido2, per.nombre;
+-- 2.	Devuelve un listado con los profesores/as que no están asociados a un departamento. ->Resp: no hay resultados porque todos tienen un departamento
+SELECT per.apellido1, per.apellido2, per.nombre FROM persona per LEFT JOIN profesor prof ON prof.id_profesor = per.id JOIN departamento dep ON dep.id = prof.id_departamento WHERE prof.id_profesor IS NULL;
+-- 3.	Devuelve un listado con los departamentos que no tienen profesores asociados.
+SELECT dep.id, dep.nombre AS departamento FROM persona per JOIN profesor prof ON prof.id_profesor = per.id RIGHT JOIN departamento dep ON dep.id = prof.id_departamento WHERE prof.id_departamento IS NULL;
+-- 4. Devuelve un listado con los profesores/as que no imparten ninguna asignatura.
+SELECT per.nombre, per.apellido1, per.apellido2, prof.id_profesor FROM persona per JOIN profesor prof ON prof.id_profesor = per.id LEFT JOIN asignatura asig ON asig.id_profesor = prof.id_profesor WHERE asig.id_profesor IS NULL;
+-- 5.	Devuelve un listado con las asignaturas que no tienen un profesor/a asignado.
+SELECT a.nombre AS asignatura FROM asignatura a LEFT JOIN profesor p ON a.id_profesor = p.id_profesor WHERE p.id_profesor IS NULL; 
+-- 6.	🕰️🕰️APARCADO(NO ME SALE)->Devuelve un listado con todos los departamentos que no han impartido asignaturas en ningún curso escolar.
+SELECT dep.nombre FROM departamento dep  LEFT JOIN profesor prof ON dep.id = prof.id_departamento JOIN asignatura asig ON asig.id_profesor = prof.id_profesor JOIN persona per ON per.id = prof.id_profesor JOIN alumno_se_matricula_asignatura am 	ON am.id_alumno = per.id JOIN curso_escolar ce ON ce.id = am.id_curso_escolar WHERE prof.id_profesor IS NULL AND ce.id IS NULL;
+	/*Consultas resumen: */
+-- 1.	Devuelve el número total de alumnos que existen.
+SELECT COUNT(pr.tipo) AS num_alumnos FROM persona pr WHERE pr.tipo = 'alumno';
+-- 2.	Calcula cuántos alumnos nacieron en 1999.
+SELECT COUNT(pr.tipo) AS num_alumnos FROM persona pr WHERE pr.fecha_nacimiento LIKE '%1999%';
+-- 3.	Calcula cuántos/as profesores/as hay en cada departamento. El resultado sólo debe mostrar dos columnas, una con el nombre del departamento y otra con el número de profesores/as que hay en ese departamento. El resultado sólo debe incluir los departamentos que tienen profesores/as asociados y deberá estar ordenado de mayor a menor por el número de profesores/as.
+SELECT dep.nombre AS nombre_departamento, COUNT(per.id) AS numero_profesores FROM departamento dep JOIN profesor prof ON prof.id_departamento = dep.id JOIN persona per ON per.id = prof.id_profesor GROUP BY dep.nombre ORDER BY numero_profesores DESC;
+-- 4.	Devuelve un listado con todos los departamentos y el número de profesores/as que hay en cada uno de ellos. Ten en cuenta que pueden existir departamentos que no tienen profesores/as asociados/as. Estos departamentos también deben aparecer en el listado.
+SELECT dep.nombre AS nom_departamento, COUNT(prof.id_departamento) AS num_profesores FROM departamento dep LEFT JOIN profesor prof  ON prof.id_departamento = dep.id GROUP BY nom_departamento;
+-- 5.	Devuelve un listado con el nombre de todos los grados existentes en la base de datos y el número de asignaturas que tiene cada uno. Ten en cuenta que pueden existir grados que carecen de asignaturas asociadas. Estos grados también deben aparecer en el listado. El resultado deberá estar ordenado de mayor a menor por el número de asignaturas.
+SELECT g.nombre AS nombre_grado, COUNT(asig.id_grado) AS num_asignaturas FROM grado g LEFT JOIN asignatura asig ON asig.id_grado = g.id GROUP BY nombre_grado ORDER BY num_asignaturas DESC;
+-- 6.	Devuelve un listado con el nombre de todos los grados existentes en la base de datos y el número de asignaturas que tiene cada uno, de los grados que tengan más de 40 asignaturas asociadas.
+SELECT grado.nombre, COUNT(asignatura.id) AS numero_asignaturas FROM grado JOIN asignatura ON grado.id = asignatura.id_grado GROUP BY grado.nombre HAVING COUNT(asignatura.id) > 40 ORDER BY numero_asignaturas DESC;
+-- 7.	Devuelve un listado que muestre el nombre de los grados y la suma del número total de créditos existentes para cada tipo de asignatura. El resultado debe tener tres columnas: nombre del grado, tipo de asignatura y la suma de los créditos de todas las asignaturas que existen de este tipo.
+SELECT g.nombre AS nom_grado, asig.tipo AS tipo_asignatura ,sum(asig.creditos) AS total_creditos FROM grado g JOIN asignatura asig ON g.id = asig.id_grado GROUP BY nom_grado, tipo_asignatura;
+-- 8.	Devuelve un listado que muestre cuántos/as alumnos/as se han matriculado de alguna asignatura en cada uno de los cursos escolares. El resultado deberá mostrar dos columnas, una columna con el año de inicio del curso escolar y otra con el número de alumnos matriculados/as.
+SELECT ce.anyo_inicio AS anyo_incio_curso, COUNT(am.id_alumno) AS num_alumnos FROM alumno_se_matricula_asignatura am  JOIN asignatura asig ON asig.id = am.id_asignatura JOIN curso_escolar ce ON ce.id = am.id_curso_escolar GROUP BY anyo_incio_curso;
+-- 9.	Devuelve un listado con el número de asignaturas que imparte cada profesor/a. El listado debe tener en cuenta a aquellos profesores/as que no imparten ninguna asignatura. El resultado mostrará cinco columnas: id, nombre, primer apellido, segundo apellido y número de asignaturas. El resultado estará ordenado de mayor a menor por el número de asignaturas.
+SELECT prof.id_profesor, per.nombre, per.apellido1, per.apellido2, COUNT(asig.id_profesor) num_asignaturas FROM profesor prof LEFT JOIN asignatura asig ON asig.id_profesor=prof.id_profesor JOIN persona per ON per.id = prof.id_profesor GROUP BY prof.id_profesor ORDER BY num_asignaturas DESC;
+-- 10.	Devuelve todos los datos del alumno más joven.
+SELECT * FROM persona WHERE tipo = 'alumno' ORDER BY fecha_nacimiento ASC LIMIT 1;
+-- 11.	Devuelve un listado con los profesores/as que tienen un departamento asociado y que no imparten ninguna asignatura.
+SELECT per.id, per.nombre, per.apellido1, per.apellido2 FROM persona per JOIN profesor prof ON per.id = prof.id_profesor LEFT JOIN asignatura asig ON prof.id_profesor = asig.id_profesor JOIN departamento dep ON dep.id = prof.id_departamento WHERE asig.id IS NULL;
